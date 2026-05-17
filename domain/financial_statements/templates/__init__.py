17099b19dead5b1e8e998bf521835f7f6ddbb59b
xofisamba <xofisamba@gmail.com>
2026-05-17 11:32:32 +0200

    Add financial statements templates package

---
+++ domain/financial_statements/templates/__init__.py
@@ -0,0 +1,11 @@
+"""Financial statement assembly templates."""
+
+from domain.financial_statements.templates.croatia import (
+    CroatiaFinancialStatementsTemplate,
+    build_croatia_financial_statements_config,
+)
+
+__all__ = [
+    "CroatiaFinancialStatementsTemplate",
+    "build_croatia_financial_statements_config",
+]
