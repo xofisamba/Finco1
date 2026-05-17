e781ce50ef83a3db582b0bed4a5eccd8669d566e
xofisamba <xofisamba@gmail.com>
2026-05-17 12:14:23 +0200

    Expose financial statements Excel audit export

--- domain/financial_statements/__init__.py
+++ domain/financial_statements/__init__.py
@@ -1,6 +1,7 @@
 """Offline financial statement assembly layer."""
 
 from domain.financial_statements.assembly import assemble_financial_statements
+from domain.financial_statements.export_excel import export_financial_statements_audit_workbook
 from domain.financial_statements.inputs import FinancialStatementsConfig
 from domain.financial_statements.result import (
     BalanceSheetPeriodResult,
@@ -26,4 +27,5 @@ __all__ = [
     "TaxBridgePeriodResult",
     "TaxBridgeResult",
     "assemble_financial_statements",
+    "export_financial_statements_audit_workbook",
 ]
